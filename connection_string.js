4a62cb2871fa44a3f7176478bd9b70d047aa9ab0
kabelo385 <kabeloxabendlini385@gmail.com>
2026-02-27 21:28:14 +0200

    Remove env file

--- connection_string.js
+++ connection_string.js
@@ -1,13 +1,12 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.9h8lq.mongodb.net/?retryWrites=true&w=majority";
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGO_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
