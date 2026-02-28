2a3ad622070086655313ea4c18e8f9265dfd1e58
kabelo385 <kabeloxabendlini385@gmail.com>
2026-02-28 13:35:59 +0200

    first commit

--- server.js
+++ server.js
@@ -4,16 +4,23 @@ import moviesRouter from "./api/movies.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://sample-mflix-frontend.onrender.com"
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-app.use("/api/v1/movies", moviesRouter)
-app.use("/api/v1/movies/review", moviesRouter) 
+app.use("/api/v1/movies", moviesRouter);
+app.use("/api/v1/movies/review", moviesRouter);
 
-// Express 5–safe 404 handler
 app.use((req, res) => {
   res.status(404).json({ error: "not found" });
 });
 
 export default app;
-
